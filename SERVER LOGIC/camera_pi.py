3ca32f3cb33cd03304a18133372cc5ef90e112f8
Mikey1213 <75994798+Mikey1213@users.noreply.github.com>
2023-10-25 01:49:59 -0400

    Update camera_pi.py

--- SERVER LOGIC/camera_pi.py
+++ SERVER LOGIC/camera_pi.py
@@ -16,6 +16,12 @@ References:
 Additional Notes:
 
 """
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as pl
+
 
 
 class Camera(object):
@@ -23,3 +29,35 @@ class Camera(object):
 
     def init(self):
         pass
+
+    server_socket = socket.socket()
+    server_socket.bind(('192.168.1.159',8000))
+    server_socket.listen(0)
+
+    connection = server_socket.accept()[0].makefile('rb')
+
+    try:
+            img = None
+            while True:
+                image_len = struct.unpack('<L',connection.read(struct.calcsize('<L')))[0]
+                if not image_len:
+                    break
+                
+                image_stream = io.BytesIO()
+                image_stream.write(connection.read(image_len))
+
+                image_stream.seek(0)
+                image = Image.open(image_stream)
+
+                if img is None:
+                      img = pl.imshow(image)
+                else:
+                    img.set_data
+
+                pl.pause(0.01)
+                pl.draw()
+
+                print('Image is %dx%d' % image.size)
+                image.verify()
+                print('Image is verified')
+    finally:
