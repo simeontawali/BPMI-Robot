af5d503992c7ff047b61cc0ab4a68405ebedb000
simeontawali <tiwarisa20@gcc.edu>
2023-11-09 18:28:53 -0500

    changes

--- TESTING/pc.py
+++ TESTING/pc.py
@@ -1,25 +1,54 @@
-from controller import XInput
 import socket
-import os
+import struct
+from controller import XInput
 
-HOST = '192.168.137.57'  # The IP address of your Raspberry Pi
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = '192.168.137.57'
+PORT = 65432
 
 xi = XInput()
 
+class ControllerData:
+    def __init__(self):
+        self.wButtons = 0
+        self.bLeftTrigger = 0
+        self.bRightTrigger = 0
+        self.sThumbLX = 0
+        self.sThumbLY = 0
+        self.sThumbRX = 0
+        self.sThumbRY = 0
+
+    def to_binary(self):
+        return struct.pack('iiiiiii', self.wButtons, self.bLeftTrigger, self.bRightTrigger, self.sThumbLX, self.sThumbLY, self.sThumbRX, self.sThumbRY)
+
+
+def convert(controller_data):
+    controller = ControllerData()
+    controller.wButtons = controller_data['wButtons']
+    controller.bLeftTrigger = controller_data['bLeftTrigger']
+    controller.bRightTrigger = controller_data['bRightTrigger']
+    controller.sThumbLX = controller_data['sThumbLX']
+    controller.sThumbLY = controller_data['sThumbLY']
+    controller.sThumbRX = controller_data['sThumbRX']
+    controller.sThumbRY = controller_data['sThumbRY']
+    return controller
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    controller = ControllerData()
     for x in range(4):
         try:
             print(f"Reading input from controller {x}")
-            data = str(xi.GetState(x)).encode('utf-8')
-            s.sendall(data)
+            state = xi.GetState(x)
+            controller = convert(state)
+            serialized_data = controller.to_binary()
+            s.sendall(serialized_data)
         except Exception as e:
             print(f"Controller {x} not available: {e}")
 
     print("Reading all inputs from gamepad 0")
     while True:
-        data = str(xi.GetState(0)).encode('utf-8')
-        os.system('cls' if os.name == 'nt' else 'clear')  # Clear the terminal
-        print(data)
-        s.sendall(data)
+        state = xi.GetState(0)
+        custom_data = convert(state)
+        serialized_data = custom_data.to_binary()  # Serialize the data
+        s.sendall(serialized_data)
