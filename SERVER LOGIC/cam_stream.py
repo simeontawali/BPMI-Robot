d6f5d36da0a99d6be458b5e321a6f75bfef87942
simeontawali <tiwarisa20@gcc.edu>
2023-11-10 06:35:30 -0500

    created webstreaming

--- SERVER LOGIC/cam_stream.py
+++ SERVER LOGIC/cam_stream.py
@@ -1,3 +1,21 @@
+"""
+BPMI Robotic Annular Pipe Sanitization System
+File Name: cam_stream.py
+Date Created: 11/09/2023 SAT
+Date Last Modified: 11/09/2023 SAT
+Description: MJPEG Web Streaming
+Verion: 1.0.1
+Authors: Gomez, Tiwari, Bennett
+
+Build Notes: First Finished Implementation
+
+Dependencies: picamera
+
+References: https://picamera.readthedocs.io/en/release-1.13/recipes2.html#web-streaming
+
+Additional Notes:
+
+"""
 import io
 import picamera
 import logging
@@ -17,6 +35,7 @@ PAGE="""\
 </html>
 """
 
+# Class to handle the streaming output from the camera
 class StreamingOutput(object):
     def __init__(self):
         self.frame = None
@@ -24,6 +43,7 @@ class StreamingOutput(object):
         self.condition = Condition()
 
     def write(self, buf):
+        # Check if the buffer starts with the JPEG start marker
         if buf.startswith(b'\xff\xd8'):
             # New frame, copy the existing buffer's content and notify all
             # clients it's available
@@ -34,13 +54,16 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
+# Class to handle HTTP requests
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
+            # Redirect root path to /index.html
             self.send_response(301)
             self.send_header('Location', '/index.html')
             self.end_headers()
         elif self.path == '/index.html':
+            # Serve the main HTML page
             content = PAGE.encode('utf-8')
             self.send_response(200)
             self.send_header('Content-Type', 'text/html')
@@ -48,6 +71,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(content)
         elif self.path == '/stream.mjpg':
+            # Set up headers for MJPEG streaming
             self.send_response(200)
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
@@ -57,8 +81,10 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             try:
                 while True:
                     with output.condition:
+                        # Wait for a new frame to be available
                         output.condition.wait()
                         frame = output.frame
+                    # Send the frame as part of the MJPEG stream
                     self.wfile.write(b'--FRAME\r\n')
                     self.send_header('Content-Type', 'image/jpeg')
                     self.send_header('Content-Length', len(frame))
@@ -70,19 +96,27 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
         else:
+            # Return a 404 error for unknown paths
             self.send_error(404)
             self.end_headers()
 
+# Class for the streaming server
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
+# Set up the camera with resolution and framerate
 with picamera.PiCamera(resolution='1280x1024', framerate=30) as camera:
+    # Create a StreamingOutput instance to handle the camera output
     output = StreamingOutput()
+    # Start recording with MJPEG format
     camera.start_recording(output, format='mjpeg')
     try:
+        # Set up the address and create the streaming server
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler)
+        # Start serving indefinitely
         server.serve_forever()
     finally:
+        # Stop recording when the program exits
         camera.stop_recording()
