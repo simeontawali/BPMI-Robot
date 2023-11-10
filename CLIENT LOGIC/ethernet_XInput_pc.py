feef27d92145c001df2df31f01ab1361a7af785a
simeontawali <tiwarisa20@gcc.edu>
2023-11-09 20:39:27 -0500

    HUGE UPDATE
    massive changes. Ethernet controller is now fully functional. Servos and leds can be controlled by the pi using controller.

---
+++ CLIENT LOGIC/ethernet_XInput_pc.py
@@ -0,0 +1,69 @@
+import socket
+import pickle
+from XInput import *
+import time
+
+# logic for controlling the game or robot
+def control_game(controller_values):
+    # Example logic
+    left_thumb_x, left_thumb_y = controller_values['left_thumb']
+    right_thumb_x, right_thumb_y = controller_values['right_thumb']
+    left_trigger, right_trigger = controller_values['triggers']
+    buttons_pressed = controller_values['buttons']
+
+    # Insert your control logic here
+    # Example: Moving a character based on the left thumbstick
+    print(f"Left Thumb: ({left_thumb_x}, {left_thumb_y})")
+
+    # Example: Shooting based on the right trigger
+    if right_trigger > 0.5:
+        print("Shooting!")
+
+    # Example: Performing an action based on button press
+    if 'A' in buttons_pressed:
+        print("Performing action!")
+
+# IP and port of the Raspberry Pi Zero
+HOST = '192.168.137.57'  # Change this to the IP of your Pi Zero
+PORT = 12345
+
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+# Your main loop
+while True:
+    events = get_events()
+    for event in events:
+        controller_values = {
+            'left_thumb': (0, 0),
+            'right_thumb': (0, 0),
+            'triggers': (0, 0),
+            'buttons': set()
+        }
+
+        if event.type == EVENT_STICK_MOVED:
+            if event.stick == LEFT:
+                controller_values['left_thumb'] = (event.x, event.y)
+            elif event.stick == RIGHT:
+                controller_values['right_thumb'] = (event.x, event.y)
+
+        elif event.type == EVENT_TRIGGER_MOVED:
+            if event.trigger == LEFT:
+                controller_values['triggers'] = (event.value, controller_values['triggers'][1])
+            elif event.trigger == RIGHT:
+                controller_values['triggers'] = (controller_values['triggers'][0], event.value)
+
+        elif event.type == EVENT_BUTTON_PRESSED:
+            controller_values['buttons'].add(event.button)
+
+        elif event.type == EVENT_BUTTON_RELEASED:
+            if event.button in controller_values['buttons']:
+                controller_values['buttons'].remove(event.button)
+
+        # Send the controller_values dictionary to the Raspberry Pi Zero
+        data = pickle.dumps(controller_values)
+        s.sendall(data)
+
+    time.sleep(0.1)  # Adjust the sleep duration as needed
---
+++ SERVER LOGIC/ethernet_controller_pi.py
@@ -0,0 +1,83 @@
+import socket
+import pickle
+import os
+import RPi.GPIO as GPIO
+import time
+
+# IP and port of the Raspberry Pi Zero
+HOST = '0.0.0.0'  # Listen on all available interfaces
+PORT = 12345
+
+led = 18
+servo1 = 16
+servo2 = 22
+GPIO.setmode(GPIO.BOARD) # for GPIO Numberng choose BCM, for pin numbering choose BOARD
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.setup(servo1, GPIO.OUT)
+p = GPIO.PWM(servo1, 100)
+
+GPIO.setup(servo2, GPIO.OUT)
+p2 = GPIO.PWM(servo2, 100)
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+
+# Accept a connection
+conn, addr = s.accept()
+
+
+# logic for controlling the game or robot
+def control(controller_values):
+    # Example logic
+    left_thumb_x, left_thumb_y = controller_values['left_thumb']
+    right_thumb_x, right_thumb_y = controller_values['right_thumb']
+    left_trigger, right_trigger = controller_values['triggers']
+    buttons_pressed = controller_values['buttons']
+
+    # Insert your control logic here
+    # Example: Moving a character based on the left thumbstick
+    #print(f"Left Thumb: ({left_thumb_x}, {left_thumb_y})")
+
+    # Example: Shooting based on the right trigger
+    if right_trigger > 0:
+        p.start(50)
+        print("right trigger")
+    else:
+        p.stop()
+    
+    if left_trigger > 0:
+        p2.start(50)
+        print("left trigger")
+    else:
+        p2.stop()
+
+    # Example: Performing an action based on button press
+    if 'A' in buttons_pressed:
+        print("LED ON")
+        GPIO.output(led, GPIO.HIGH) # Toggle the LED
+
+    if 'B' in buttons_pressed:
+        print("LED OFF")
+        GPIO.output(led, GPIO.LOW)
+
+
+while True:
+    # Receive the data from the sender
+    data = conn.recv(4096)
+
+    if not data:
+        break
+
+    # Unpickle the received data
+    controller_values = pickle.loads(data)
+
+    # Process the received data
+    # Add your processing logic here
+    control(controller_values)
+
+conn.close()
+GPIO.cleanup()
+
