54e57cea2d2d301d61804e6038f28511b6ae11dc
bennettke20 <95378515+bennettke20@users.noreply.github.com>
2024-05-08 21:54:12 -0400

    comments added

--- MAIN/FINAL/conn_manager.py
+++ MAIN/FINAL/conn_manager.py
@@ -1,26 +1,34 @@
+"""
+BPMI Control Data Management Scripts
+
+This script initializes the socket connection to the user application through the network class,
+interprets the controller data through the controller class, and updates the robot hardware through
+the robot class.
+
+"""
+
 from network import NetworkCommunication
 from robot import RobotControl
 from controller import Controller
 import json
-# lights GPIO 21,20,18
 
 def main():
-    network = NetworkCommunication()
-    robot = RobotControl()
-    controller = Controller()
+    network = NetworkCommunication() # receives/sends communications from user application
+    robot = RobotControl() # interfaces with robot hardware
+    controller = Controller() # interpret and store controller data
 
     try:
-        conn = network.accept_connection()
+        conn = network.accept_connection() # establish connection
         while True:
 
             try:
-                controller_values = network.receive_control_data(conn)
+                controller_values = network.receive_control_data(conn) # receive controller data
                 if controller_values is None:
                     pass
                 for controller_val in controller_values:
-                    controller.update_state(controller_val)
-                    indicators = robot.update_control(controller)
-                    #network.send_data(indicators) # not debugged
+                    controller.update_state(controller_val) # update controller data
+                    indicators = robot.update_control(controller) # update robot hardware control
+                    #network.send_data(indicators) # not debugged, send robot status data back to user application
             except json.JSONDecodeError as e:
                 print(e)
                 print('Detected JSON decode error')
